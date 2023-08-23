748c6dc777eb87ff72c40fee300dec9293fd8957
saadelattar <sa.elattar.ad@gmail.com>
2023-08-23 22:17:57 +0100

    init commit poc

---
+++ src/main/java/io/daasrattale/cloudnativepoc/book/BookRepository.java
@@ -0,0 +1,6 @@
+package io.daasrattale.cloudnativepoc.book;
+
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+public interface BookRepository extends R2dbcRepository<Book, Long> {
+}
---
+++ src/test/java/io/daasrattale/cloudnativepoc/CloudNativePocApplicationTests.java
@@ -0,0 +1,13 @@
+package io.daasrattale.cloudnativepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNativePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
